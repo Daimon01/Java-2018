9700f95b24f4f014c618ae22f5dd013eb85381e1
Daimon01 <Dimas.1990@inbox.ru>
2018-03-15 20:46:16 +0600

    lab01

---
+++ src/lab12/task5164.java
@@ -0,0 +1,45 @@
+package lab12;
+
+import laboratorki.lab01;
+
+public class task5164 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ура заработало!!!");
+        System.out.println("Задача 1860");
+        lab01.task1860("знаниям");
+        lab01.task1860("цели");
+        lab01.task1860("результату");
+        System.out.println("Задача 4764");
+        lab01.task4764("Трудности");
+        lab01.task4764("Загадки");
+        lab01.task4764("Сложные задачи");
+        System.out.println("Задача 2429");
+        lab01.task2429("просто!");
+        lab01.task2429("интересно!");
+        lab01.task2429("перспективно!");
+        System.out.println("Задача 7472");
+        lab01.task7472("начало", "конец");
+        lab01.task7472("сила", "знание");
+        lab01.task7472("курица", "яйцо");
+        lab01.task7472("шаг", "шаг");
+        System.out.println("Задача 2959");
+        lab01.task2959("123456789");
+        lab01.task2959("fa93f");
+        System.out.println("Задание 7271");
+        lab01.task7271(5,7);
+        lab01.task7271(-1,1000);
+        System.out.println("Задание 2632");
+        lab01.task2632(50,50,40,"Yellow");
+        lab01.task2632(400,300,200,"Green");
+        lab01.task2632(254,356,100,"#cc6600");
+        System.out.println("Задание 4343");
+        lab01.task4343("test","pass","localhost",5432,"my_db");
+        lab01.task4343("admin","7e8efd48d69c","production-node1",5432,"bank");
+        lab01.task4343("akryukov","123","localhost",0000,"bank");
+    }
+
+
+
+}
---
+++ src/laboratorki/lab01.java
@@ -0,0 +1,32 @@
+package laboratorki;
+
+public class lab01 {
+    public static void task1860(String a) {
+        System.out.println("Мы стремимся к " + a);
+    }
+
+    public static void task4764(String a) {
+        System.out.println(a + " нас не остановят!");
+    }
+    public static void task2429(String a) {
+        System.out.println("Программирование это " +a);
+    }
+    public static void task7472(String a, String b){
+        System.out.println(b + " " + a);
+    }
+    public static void task2959(String a){
+        System.out.println("SELECT first_name, last_name, group\n" +
+                "FROM students WHERE student_id = '"+a+"'");
+    }
+    public static void task7271(Integer a, Integer b){
+        System.out.println("INSERT INTO points (x, y) VALUES ('"+a+"', '"+b+"')");
+    }
+    public static void task2632(Integer a, Integer b, Integer c, String d){
+        System.out.println("<circle cx=\""+a+"\" cy=\""+b+"\"\n" +
+                "r=\""+c+"\" fill=\""+d+"\"/>");
+    }
+    public  static void task4343(String a, String b, String c, Integer d, String e){
+    System.out.println("User ID="+a+";Password="+b+";\n" +
+              "Host="+c+";Port="+d+";Database="+e+";");
+    }
+}
