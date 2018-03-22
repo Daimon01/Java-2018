0bd0e23f473bbf5a28a95b221313dc20b63386b7
Daimon01 <Dimas.1990@inbox.ru>
2018-03-22 21:03:45 +0600

    булеровы выражения

--- src/lab12/task2030.java
+++ src/lab12/task2030.java
@@ -1,4 +1,109 @@
 package lab12;
 
+import laboratorki.lab03;
+
 public class task2030 {
+    public static void main(String[] args) {
+        System.out.println("Задание 8878");
+        if (lab03.task8878(5, 7)) {
+            System.out.println("Второе");
+        } else {
+            System.out.println("Первое");
+        }
+        if (lab03.task8878(11, 7)) {
+            System.out.println("Второе");
+        } else {
+            System.out.println("Первое");
+        }
+        if (lab03.task8878(-2, 3)) {
+            System.out.println("Второе");
+        } else {
+            System.out.println("Первое");
+        }
+        if (lab03.task8878(-13, -18)) {
+            System.out.println("Второе");
+        } else {
+            System.out.println("Первое");
+        }
+        System.out.println("Задание 8715");
+        if (lab03.task8715(1,2,3)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(-4,0,5)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(7,4,-2)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(0,5,-7)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(4,4,4)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(-2,4,3)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(10,3,5)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        if (lab03.task8715(5,1,7)){
+            System.out.println("Выподняется");
+        }
+        else {
+            System.out.println("Не выполняется");
+        }
+        System.out.println("Задание 4847");
+        if (lab03.task4847(31,59,83)){
+            System.out.println("Среди чисел есть равные");
+        }
+        else {
+            System.out.println("Числа не равны друг другу");
+        }
+        if ((lab03.task4847(19,19,41))){
+            System.out.println("Среди чисел есть равные");
+        }
+        else{
+            System.out.println("Числа не равны друг другу");
+        }
+        if (lab03.task4847(7,11,11)){
+            System.out.println("Среди чисел есть равные");
+        }
+        else{
+            System.out.println("Числа не равны друг другу");
+        }
+        if (lab03.task4847(67,-67,67)){
+            System.out.println("Среди чисел есть равные");
+        }
+        else{
+            System.out.println("Числа не равны друг другу");
+        }
+        if (lab03.task4847(1,1,1)){
+            System.out.println("Среди чисел есть равные");
+        }
+        else{
+            System.out.println("Числа не равны друг другу");
+        }
+    }
 }
--- src/laboratorki/lab03.java
+++ src/laboratorki/lab03.java
@@ -1,4 +1,18 @@
 package laboratorki;
 
 public class lab03 {
+    public static boolean task8878(int a, int b) {
+        return a < b;
+    }
+
+    public static boolean task8715(int a, int b, int c) {
+        if (a < b) {
+            return b < c;
+        }
+        return false;
+    }
+
+    public static boolean task4847(int a, int b, int c) {
+        return (a == b || b == c || a == c);
+    }
 }
