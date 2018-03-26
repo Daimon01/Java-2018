0a9d55be7f4d77047c3c12d0bba4c64878ced7d7
Daimon01 <Dimas.1990@inbox.ru>
2018-03-26 21:04:03 +0600

    lab04

--- src/lab12/task7649.java
+++ src/lab12/task7649.java
@@ -1,5 +1,16 @@
 package lab12;
+
 import laboratorki.lab04;
 
 public class task7649 {
+    public static void main(String[] args) {
+        System.out.println("Задание 8495");
+            lab04.task8495("!");
+        System.out.println("Задание 1315");
+        lab04.task1315(17);
+        System.out.println("Задание 2475");
+        lab04.task2475(100);
+        System.out.println("Задание 5951");
+        lab04.task5951(20);
+    }
 }
--- src/laboratorki/lab04.java
+++ src/laboratorki/lab04.java
@@ -1,9 +1,28 @@
 package laboratorki;
 
 public class lab04 {
-    public static String task8495 (String a){
-        for (int i=0; i<13; i++){
-            a[i] = a;
+    public static void task8495 (String a){
+        for (int i = 0; i < 13; i++) {
+            System.out.print(a);
         }
+        System.out.println();
+    }
+    public static void task1315(int a){
+        for (int i = a; i <= 47; i++) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void task2475(long a){
+        for (int i = 101; i <= 400; i++) {
+            a = a + i;
+        }
+        System.out.println("\n"+a);
+    }
+    public static void task5951 (double a){
+        for (int i = 21; i <= 40; i++) {
+            a = Math.pow(a, 3) + Math.pow(i,3);
+        }
+        System.out.println(a);
     }
 }
